5fb8696d957f620a6afb6e7eb19ec8d53f599271
Mo-Mans0ur <Moha1998@outlook.dk>
2024-04-15 12:52:05 +0200

    11/04/2024

--- 18._Multipart-Forms/node-multipart-forms/app.js
+++ 18._Multipart-Forms/node-multipart-forms/app.js
@@ -4,7 +4,32 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 import multer from "multer";
-const upload = multer({ dest: "./uploads" });
+//const upload = multer({ dest: "./uploads" });
+import path from "path";
+console.log(path.sep);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(req);
+    console.log(file);
+
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const uniqueFileName = `${uniquePrefix}__${file.originalname}`;
+
+    cb(null, uniqueFileName);
+  },
+});
+
+function fileFilter(req, file, cb) {
+  const allowedTypes = ["image/jpeg", "image/svg", "image/png"];
+
+  console.log(file.mimetype);
+}
+
+const upload = multer({ storage, limits: { fileSize: 0.1 * 1024 * 1024 } });
 
 app.post("/form", (req, res) => {
   console.log(req.body);
@@ -12,8 +37,8 @@ app.post("/form", (req, res) => {
   res.send(req.body);
 });
 
-app.post("/fileform",upload.single('file'), (req, res) => {
-    console.log(req.body);
+app.post("/fileform", upload.single("file"), (req, res) => {
+  console.log(req.body);
   res.send({ });
 });
 
--- 18._Multipart-Forms/python-multipart-forms/main.py
+++ 18._Multipart-Forms/python-multipart-forms/main.py
@@ -1,9 +1,38 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form , File, UploadFile
+import aiofiles
+from typing import Optional
+
 
 app = FastAPI()
 
-app.post("/form")
+@app.post("/form")
 def basic_form(username: str = Form(...), password: str = Form(default="...", min_length=8)):
     print(username, password)
     return {"username": username}
     
+
+#@app.post("/fileform")
+#def file_form(file: bytes = File(...)):
+#    with open('file', 'wb') as f:
+#        f.write(file)
+
+#    return {"message": "File uploaded successfully"}
+
+        
+#@app.post("/fileform")
+#async def file_form(file: UploadFile = File(...)):
+#   contents = await file.read()
+#   print(contents)
+
+#   return {"filename": file.filename}
+
+
+@app.post("/fileform")
+async def file_form(file: UploadFile = File(...), description: Optional[str] = Form(None)):
+    safe_filename = file.filename.replace("/", "_").replace("\\", "_")
+    # with aiofiles it ensures that is asyncronous
+
+    async with aiofiles.open(safe_filename, 'wb') as f:
+        # Walrus operator
+        while content := await file.read(1024): # Read 1024 chunks
+            await f.write(content)
